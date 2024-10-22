588f57d11bcdaa293867cb0c07bf0c8aa525e588
isaac <isaac.levi@outlook.com>
2024-10-22 20:27:06 -0300

    Update API.py

--- API.py
+++ API.py
@@ -135,9 +135,84 @@ def deleteLivro():
         cursor.close()
             
 
-@app.route('/update')
-def update():
-    return
+@app.route('/updateautor', methods=['PUT']) #Atualização na tabela autor (entidade)
+def updateAutor():
+    try:
+        # Obtendo dados da requisição
+	autor_antigo = request.json['autor_antigo']
+        autor_novo = request.json['autor_novo']
+        genero = request.json['genero']
+        nacionalidade = request.json['nacionalidade']
+	
+
+        cursor = bd.cursor() #Inicia cursor
+        cursor.execute('''
+            UPDATE biblioteca.autor SET nome = %s, genero_principal = %s, nacionalidade=%s WHERE nome = %s;''', (autor_novo, genero, nacionalidade, autor_antigo))
+        bd.commit() #Confirma a atualização
+
+        return {"message": "Autor atualizado com sucesso!"}, 200
+    
+    except psycopg2.Error as e:
+        bd.rollback() #Reverte a atualização
+        return {"Erro:": str(e)}, 400
+
+    finally:
+        cursor.close()
+
+@app.route('/updatelivro', methods=['PUT'])
+def updateLivro():
+    try:
+        # Obtendo dados da requisição
+        livro_isbn = request.json['isbn']  # ISBN do livro que deseja atualizar
+        novo_nome = request.json['novo_nome']
+        novo_num_pag = request.json['novo_num_pag']
+        nova_sinopse = request.json['nova_sinopse']
+        novo_genero = request.json['novo_genero']
+        nova_class_indic = request.json['nova_class_indic']
+        novo_formato = request.json['novo_formato']
+
+        cursor = bd.cursor()
+        cursor.execute('''
+            UPDATE biblioteca.livro
+            SET nome = %s, num_pag = %s, sinopse = %s, genero = %s, class_indic = %s, formato = %s
+            WHERE isbn = %s;''',
+            (novo_nome, novo_num_pag, nova_sinopse, novo_genero, nova_class_indic, novo_formato, livro_isbn))
+        bd.commit()
+
+        return {"message": "Livro atualizado com sucesso!"}, 200
+
+    except psycopg2.Error as e:
+        bd.rollback()
+        return {"Erro:": str(e)}, 400
+
+    finally:
+        cursor.close()
+
+@app.route('/updateescreveu', methods=['PUT'])
+def updateEscreveu():
+    try:
+        # Obtendo dados da requisição
+        autor_nome = request.json['autor_nome']
+        livro_isbn = request.json['livro_isbn']
+        nova_data_escrito = request.json['nova_data_escrito']
+        nova_sequencia = request.json['nova_sequencia']
+
+        cursor = bd.cursor()
+        cursor.execute('''
+            UPDATE biblioteca.escreveu
+            SET data_escrito = %s, sequencia = %s
+            WHERE autor_nome = %s AND livro_isbn = %s;''',
+            (nova_data_escrito, nova_sequencia, autor_nome, livro_isbn))
+        bd.commit()
+
+        return {"message": "Relação 'Escreveu' atualizada com sucesso!"}, 200
+
+    except psycopg2.Error as e:
+        bd.rollback()
+        return {"Erro:": str(e)}, 400
+
+    finally:
+        cursor.close()
 
 @app.route('/leitura', methods=['GET'])
 def leitura():
